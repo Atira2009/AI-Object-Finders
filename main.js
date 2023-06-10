0065f42c3842650c307cacfd330ddb1d9530ad49
Atira2009 <77487768+Atira2009@users.noreply.github.com>
2023-06-10 13:38:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 status = ""
 hint = ""
+objects = ""
+synth = window.speechSynthesis;
+
 function setup(){
     canvas = createCanvas(380,380);
     canvas.center();
     video = createCapture(VIDEO);
     video.size(380,380);
     video.hide();
-}
-
-function draw(){
-    image(video, 0, 0, 380, 380);
 }
 
 function start(){
@@ -21,4 +20,42 @@ function start(){
 function modelLoaded(){
     console.log("Model Loaded!");
     status = true;
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380);
+    if(status !=""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i <objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            
+            fill("#FF0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%",objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label = hint){
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("object_status").innerHTML = "Object metioned is found"
+                speech = new SpeechSynthesisUtterance('Object metioned is found');
+                window.speechSynthesis.speak(speech);
+
+            }else{
+                document.getElementById("object_status").innerHTML = "Object metioned is not found"
+                speech = SpeechSynthesisUtterance('Object metioned is not found');
+                window.speechSynthesis.speak(speech);
+            }
+        }
+      }
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results
 }
